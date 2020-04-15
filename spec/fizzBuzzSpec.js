4fd6a466e38ff816b6e77762581aa397bbaef0c8
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 18:16:20 +0100

    refactor tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,22 +1,22 @@
 describe('Fizzbuzz', function() {
 
-  var fizzbuzz;
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz;
+  })
+  
 
   describe('returns FizzBuzz when given mutiple of 3 & 5', function() {
     it('FizzBuzzes for 15', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.play(15)).toEqual('FizzBuzz');
     });
 
     it('Fizzbuzzes for 60', function() {
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.play(60)).toEqual('FizzBuzz');
     });
   });
 
   describe('return Fizz when given multiple of 3', function() {
     it('Fizzes for 3', function() {
-    fizzbuzz = new Fizzbuzz();
     expect(fizzbuzz.play(3)).toEqual('Fizz');
     });
   });
