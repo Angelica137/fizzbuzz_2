6e82e985114f828612076c26756e3843fe3ac334
Angelica <Angelica137@users.noreply.github.com>
2020-04-15 17:54:35 +0100

    tdd returns fizzbuzz when mult of 3 & 5, red

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,1 +1,11 @@
-describe('Fizzbuzz', function() {});
+describe('Fizzbuzz', function() {
+
+  var fizzbuzz;
+
+  describe('returns FizzBuzz when given mutiple of 3 & 5', function() {
+    it('FizzBuzzes for 15', function() {
+      expect(fizzuzz.play(15)).toEqual('FizzBuzz');
+    })
+  })
+});
+
